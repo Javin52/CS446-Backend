9f88434bacd9821a6dea5b630825e7b274085ee0
Javin <javinmehta52@gmail.com>
2023-07-16 18:50:09 -0400

    created db schema

---
+++ CreateTables.sql
@@ -0,0 +1,75 @@
+USE CS446;
+
+CREATE TABLE user (
+    userId char(32) NOT NULL,
+    email varchar(200) NOT NULL,
+    username varchar(100) NOT NULL,
+    userPassword varchar(100),
+    PRIMARY KEY(userId),
+    UNIQUE (email)
+);
+
+CREATE TABLE followers (
+    userId char(32) NOT NULL,
+    follows char(32) NOT NULL,
+    PRIMARY KEY(userId, follows),
+    FOREIGN KEY (userId) REFERENCES user(userId) ON DELETE CASCADE,
+    FOREIGN KEY (follows) REFERENCES user(userId) ON DELETE CASCADE
+);
+
+CREATE TABLE post (
+    postId char(32) NOT NULL,
+    user char(32) NOT NULL,
+    category char(100),
+    likes int NOT NULL,
+    content varchar(500),
+    PRIMARY KEY (postId),
+    FOREIGN KEY (user) REFERENCES user(userId)
+);
+
+CREATE TABLE comments (
+    postId char(32) NOT NULL,
+    comment char(32),
+    PRIMARY KEY (postId, comment),
+    FOREIGN KEY (postId) REFERENCES post(postId) ON DELETE CASCADE,
+    FOREIGN KEY (comment) REFERENCES post(postId) ON DELETE CASCADE
+);
+
+CREATE TABLE routine (
+    routineId char(32) NOT NULL,
+    author char(32) NOT NULL,
+    routine_name varchar(100) NOT NULL,
+    PRIMARY KEY (routineId),
+    FOREIGN KEY (author) REFERENCES user(userId)
+);
+
+CREATE TABLE exercise (
+    routineId char(32) NOT NULL,
+    exerciseId char(32) NOT NULL,
+    exerciseName varchar(100) NOT NULL,
+    sets int,
+    reps int,
+    weight int,
+    weightType varchar(5),
+    duration int,
+    durationType varchar(10),
+    distanct int,
+    distanceType varchar(10),
+    PRIMARY KEY (routineId, exerciseId),
+    FOREIGN KEY (routineId) REFERENCES routine(routineId) ON DELETE CASCADE
+);
+
+CREATE TABLE postExercise (
+    postId char(32) NOT NULL,
+    routineId char(32) NOT NULL,
+    PRIMARY KEY (postId, routineId),
+    FOREIGN KEY (postId) REFERENCES post(postId) ON DELETE CASCADE,
+    FOREIGN KEY (routineId) REFERENCES routine(routineId) ON DELETE CASCADE
+);
+
+
+CREATE TABLE tutorials (
+    exercise_name varchar(100),
+    url varchar(250),
+    PRIMARY KEY (exercise_name)
+);
---
+++ README.md
@@ -0,0 +1,20 @@
+## Quick Description ##
+This is the backend code for CS 446 fitness app.
+The main content for the back end code is related to the social component of the application. 
+
+## Endpoints supported ##
+- upload routine
+- get list of user posts
+- create/edit/delete post
+- create/edit/delete comment
+- like comment
+- follow user
+- search tutorial
+
+
+## Running
+To run use 
+```
+flask --app src run
+```
+in the case the .flaskenv variables do not seem to work
